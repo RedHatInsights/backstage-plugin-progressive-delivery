a2310c71d9b4c5c40e2cd0f9dfc0a1c538636f9e
Stephen Reaves <sreaves@redhat.com>
2024-05-14 10:53:48 -0400

    Handle multiple versions

--- create_sha.sh
+++ create_sha.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-fsha=$(sha256sum frontend/backstage-plugin-progressive-delivery-backstage-plugin-progressive-delivery-frontend-v0.1.0.tgz | awk '{print $1}' | xxd -r -p | base64)
-bsha=$(sha256sum backend/backstage-plugin-progressive-delivery-backstage-plugin-progressive-delivery-backend-v0.1.0.tgz | awk '{print $1}' | xxd -r -p | base64)
+fsha=$(sha256sum frontend/backstage-plugin-progressive-delivery-backstage-plugin-progressive-delivery-frontend-*.tgz | awk '{print $1}' | xxd -r -p | base64)
+bsha=$(sha256sum backend/backstage-plugin-progressive-delivery-backstage-plugin-progressive-delivery-backend-*.tgz | awk '{print $1}' | xxd -r -p | base64)
 
 echo "Frontend: sha256-${fsha}"
 echo "Backend:  sha256-${bsha}"
