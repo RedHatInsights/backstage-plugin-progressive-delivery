0551d0cbbf299a461c8d752e995077fa415f1084
Stephen Reaves <sreaves@redhat.com>
2024-05-20 16:58:14 -0400

    Mock FS testing

--- backend/src/service/router.test.ts
+++ backend/src/service/router.test.ts
@@ -1,6 +1,7 @@
 import { getVoidLogger } from '@backstage/backend-common';
 import express from 'express';
 import request from 'supertest';
+import mock from 'mock-fs';
 
 import { createRouter } from './router';
 import { MockConfigApi } from '@backstage/test-utils';
@@ -8,8 +9,22 @@ import { MockConfigApi } from '@backstage/test-utils';
 describe('createRouter', () => {
   let app: express.Express;
   const mockConfig = new MockConfigApi({
-  app: { baseUrl: 'https://example.com' },
-});
+    app: { baseUrl: 'https://example.com' },
+    progressiveDelivery: { saasPromotionsJson: '/tmp/saas.json' },
+  });
+
+    const data =  {
+      "nodes": [
+        "{\"app\": \"foo\", \"cluster\": \"bar-stage-01\", \"commit_sha\": \"sha\", \"deployment_state\": \"success\", \"isTest\": false, \"namespace\": \"foo-stage\", \"resourceTemplate\": \"foo\", \"saas\": \"foo\", \"target\": null}",
+        "{\"app\": \"bar\", \"cluster\": null, \"commit_sha\": \"\", \"deployment_state\": \"\", \"isTest\": false, \"namespace\": null, \"resourceTemplate\": \"baz-demo\", \"saas\": \"saas-baz-demo-a\", \"target\": \"baz-demo-integration-a\"}"
+      ],
+      "edges": [
+        [
+          "{\"app\": \"foo\", \"cluster\": \"bar-stage-01\", \"commit_sha\": \"sha\", \"deployment_state\": \"success\", \"isTest\": false, \"namespace\": \"foo-stage\", \"resourceTemplate\": \"foo\", \"saas\": \"foo\", \"target\": null}",
+          "{\"app\": \"bar\", \"cluster\": null, \"commit_sha\": \"\", \"deployment_state\": \"\", \"isTest\": false, \"namespace\": null, \"resourceTemplate\": \"baz-demo\", \"saas\": \"saas-baz-demo-a\", \"target\": \"baz-demo-integration-a\"}"
+        ]
+      ]
+    };
 
   beforeAll(async () => {
     const router = await createRouter({
@@ -17,6 +32,14 @@ describe('createRouter', () => {
       config: mockConfig,
     });
     app = express().use(router);
+
+    mock({
+      '/tmp/saas.json' : JSON.stringify(data)
+    });
+  });
+
+  afterAll(async () => {
+    mock.restore();
   });
 
   beforeEach(() => {
@@ -30,5 +53,14 @@ describe('createRouter', () => {
       expect(response.status).toEqual(200);
       expect(response.body).toEqual({ status: 'ok' });
     });
+  });
+
+  describe('GET /topo', () => {
+    it('returns data', async () => {
+      const response = await request(app).get('/topo');
+
+      expect(response.status).toEqual(200);
+      expect(response.body).toEqual(data);
+    })
   });
 });
