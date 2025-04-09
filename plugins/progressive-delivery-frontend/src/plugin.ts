9b01b75e029f98bfee4334a6f860433be6970c8a
Matt Knop <mknop@redhat.com>
2025-04-09 08:32:17 -0600

    started refactor

--- plugins/progressive-delivery-frontend/src/plugin.ts
+++ plugins/progressive-delivery-frontend/src/plugin.ts
@@ -4,16 +4,14 @@ import { rootRouteRef } from './routes';
 
 export const progressiveDeliveryPlugin = createPlugin({
   id: 'progressive-delivery',
-  routes: {
-    root: rootRouteRef,
-  },
 });
 
-export const ProgressiveDeliveryPage = progressiveDeliveryPlugin.provide(
-  createRoutableExtension({
-    name: 'ProgressiveDeliveryPage',
-    component: () =>
-      import('./components/ProgressiveDeliveryComponent').then(m => m.TopologyComponent),
-    mountPoint: rootRouteRef,
+export const EntityProgressiveDeliveryContent = progressiveDeliveryPlugin.provide(
+  createComponentExtension({
+    name: 'EntityProgressiveDeliveryContent',
+    component: {
+      lazy: () => import('./components/ProgressiveDeliveryComponent').then(m => m.ProgressiveDeliveryComponent),
+    },
   }),
 );
+
