b6978c56fde88c1e5af2454b58212a0732cddc74
Matt Knop <mknop@redhat.com>
2025-05-07 12:09:16 -0600

    update type config file to remove progressive-delivery type

--- frontend/config.d.ts
+++ frontend/config.d.ts
@@ -1,9 +1,1 @@
-export interface Config {
-  "progressive-delivery": {
-    /**
-     * The public absolute root URL that the frontend.
-     * @visibility frontend
-     */
-    "saas-promotions-json": string;
-  }
-}
+export interface Config {}
